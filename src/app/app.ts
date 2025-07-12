e7bebb8528266a1259707a2e9f2d8dfd91fa5778
Ammar Ahmed <ammar0210@hotmail.com>
2025-07-11 22:41:36 -0500

    Initial Commit.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {BasicInformation} from './basic-information/basic-information';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, BasicInformation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/basic-information/basic-information.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-information',
+  imports: [],
+  standalone: true,
+  templateUrl: './basic-information.html',
+  styleUrl: './basic-information.css'
+})
+export class BasicInformation {
+
+}
