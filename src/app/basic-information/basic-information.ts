37ba4159994929367a0081822d9ca4cbfaabf4aa
Ammar Ahmed <ammar0210@hotmail.com>
2025-07-12 19:03:18 -0500

    Updated UI

--- src/app/basic-information/basic-information.ts
+++ src/app/basic-information/basic-information.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-basic-information',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './basic-information.html',
-  styleUrls: ['./basic-information.css']
+  styleUrl: './basic-information.css'
 })
 export class BasicInformation {
-
-  ngOnInit(): void {
-  }
-
+  showFunFact: boolean = false;
 }
